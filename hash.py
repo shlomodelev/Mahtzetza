de7ba85c94586e2743617898ee90c4573a973fec
shlomodelev <shlomodelev@gmail.com>
2017-07-05 16:45:44 +0300

    first hash collusion now occurs at 600H

--- hash.py
+++ hash.py
@@ -1,5 +1,5 @@
-from math import sin
-
+from math import sin, cos
+from copy import copy
 
 def get(b_arr):
     ha_arr = [0x12340FFE, 0xA3B2EF56, 0xCD53CA23, 0xE57EB124]
@@ -7,21 +7,55 @@ def get(b_arr):
     unit = 0xFFFFFFFF
     result_length = unit*length
 
-    for i, byte in enumerate(b_arr):
-        curr_ind = (length - i) % length
-        ha_arr[curr_ind] *= (byte | int(100.0*sin(i)))
+    for byte in b_arr:
+        curr_ind = byte % length
+        #print 5 * byte + 2
+        #print byte >> 1
+        ha_arr[curr_ind] *= byte >> 1| int(85.0*sin((5*byte+2)/BYTE))
+        #print ha_arr[curr_ind]
+        #print 7*byte - 3
+        #print ~byte >> 2
+        ha_arr[curr_ind] += ~byte >> 2 ^ int(97.0*cos((7*byte-3)/BYTE))* unit / 2**8
+        #print ha_arr[curr_ind]
         ha_arr[curr_ind] %= unit
         ha_arr[0], ha_arr[1], ha_arr[2], ha_arr[3] = ha_arr[2], ha_arr[3], ha_arr[0], ha_arr[1]
+        #print ha_arr
 
     return reduce(lambda x, y: (x*x+y*y) % result_length, ha_arr)
 
 
+BYTE = 0x100
+
+def int_to_bytes(value):
+    result = []
+    value = copy(value)
+
+    while value > 0:
+        result.append(value % BYTE)
+        value /= BYTE
+
+    result.reverse()
+
+    return result
+
+
+
 def main():
-    s_list = ['Shlomo Modelevsky', 'Rhlomo Modelevsky', 'Rhlomo modelevskz', 'Rglomo modelevskz',
-			  'Rglomo modelevslz', 'Rgkomo modelevslz']
-    for s in s_list:
-		print '{0} : {1}'.format(s, format(get(bytearray(s, 'utf-8')), '02x'))
-		print [x for x in bytearray(s, 'utf-8')]
+    #print get(int_to_bytes(0x55))
+    #print 'SHLOMO'
+    #print get(int_to_bytes(0x59))
+    #exit()
+    achieved_hashes = {}
+
+    n = 10000
+    for i in xrange(n):
+        hash_value = get(int_to_bytes(i))
+        if hash_value not in achieved_hashes:
+            achieved_hashes[hash_value] = i
+        else:
+            # print the first collusion:
+            print '{0} : {1}, {2}'.format(hex(hash_value), hex(achieved_hashes[hash_value]), hex(i))
+            break
 
 
 if __name__ == '__main__':
